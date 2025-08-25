4fbbfdb2c48ab34f8fb9eef9dcfaa24f52abe938
Ginam01 <dbrlska0105@gmail.com>
2025-08-25 14:22:27 +0900

    update

---
+++ src/main/java/kr/co/iei/AllPage/model/dao/AllPageDao.java
@@ -0,0 +1,7 @@
+package kr.co.iei.AllPage.model.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AllPageDao {
+}
---
+++ src/main/java/kr/co/iei/AllPage/model/service/AllPageService.java
@@ -0,0 +1,12 @@
+package kr.co.iei.AllPage.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.iei.AllPage.model.dao.AllPageDao;
+
+@Service
+public class AllPageService {
+	@Autowired
+	private AllPageDao allpagedao;
+}
