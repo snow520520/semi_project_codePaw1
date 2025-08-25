872eadc54c1c2ae2c4769a33d137e2ef6fa9cbab
Ginam01 <dbrlska0105@gmail.com>
2025-08-25 17:10:49 +0900

    commit

--- src/main/java/kr/co/iei/AllPage/model/service/AllPageService.java
+++ src/main/java/kr/co/iei/AllPage/model/service/AllPageService.java
@@ -4,9 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.iei.AllPage.model.dao.AllPageDao;
+import kr.co.iei.AllPage.model.vo.AllPage;
 
 @Service
 public class AllPageService {
 	@Autowired
 	private AllPageDao allpagedao;
+
 }
