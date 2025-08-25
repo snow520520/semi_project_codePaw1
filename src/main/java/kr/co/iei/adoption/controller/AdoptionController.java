334f99d349179b58de0152d84e1ec70a5e3472ca
xfhlut <iteawondodls@gmail.com>
2025-08-25 13:34:25 +0900

    asd

--- src/main/java/kr/co/iei/adoption/controller/AdoptionController.java
+++ src/main/java/kr/co/iei/adoption/controller/AdoptionController.java
@@ -2,9 +2,12 @@ package kr.co.iei.adoption.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.iei.adoption.model.service.AdoptionService;
+import kr.co.iei.adoption.model.vo.AdoptionListData;
 
 @Controller
 @RequestMapping(value="/adoption")
@@ -12,4 +15,12 @@ public class AdoptionController {
 
 	@Autowired
 	private AdoptionService adoptionService;
+	
+	@GetMapping(value="/list")
+	public String adoptionList(int reqPage, Model model) {
+		AdoptionListData ald = adoptionService.selectAdoptionList(reqPage);
+		model.addAttribute("list", ald.getList());
+		model.addAttribute("pageNavi", ald.getPageNavi());
+		return "adoption/list";
+	}
 }
