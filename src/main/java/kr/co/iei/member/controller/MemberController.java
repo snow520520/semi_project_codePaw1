02658581ca28140e718c955198a302c319e8233f
yeah <skjb6996@naver.com>
2025-08-25 18:10:04 +0900

    commit

--- src/main/java/kr/co/iei/member/controller/MemberController.java
+++ src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -27,20 +27,32 @@ public class MemberController {
 		//select * from member_tbl where member_id = memberId and member_pw = memberPw
 		Member member = memberService.login(m);
 		System.out.println(member);
-		///dsdasda
+
 		if(member == null) {
 			//member == null 이면 로그인이 안된경우 (id or pw가 틀렸다는 문구 다시 확인하세요)
-			
+			model.addAttribute("title", "로그인 실패");
+			model.addAttribute("text", "아이디와 비밀번호를 확인해주세요.");
+			model.addAttribute("icon", "error");
+			model.addAttribute("loc", "/member/loginFrm");
+			// alert css/js 넣고 처리해야함
 		}else {
 			//member != null 로그인 된 경우
-			if(member.getMemberLevel() == 1) {
-				//member != null or memberLevel == 1이면 (관리자 권한 (마이페이지 or 관리자 페이지))
+			if(member.getMemberLevel() == 3) {
+				//member != null or memberLevel == 3이면 (정회원(입양하지 않은 회원) 마이페이지에 회원정보 수정할수 있게끔만)
+				session.setAttribute("member", member);
 			}else if(member.getMemberLevel() == 2) {
 				//member != null or memberLevel == 2이면 (입양한 회원 (마이페이지에 입양 후기쓴 글 or 입양신청서 작성 시 수락 / 회원정보 수정할 수 있게))
-			}else if(member.getMemberLevel() == 3) {
-				//member != null or memberLevel == 3이면 (정회원(입양하지 않은 회원) 마이페이지에 회원정보 수정할수 있게끔만)
+				session.setAttribute("member", member);
+			}else if(member.getMemberLevel() == 1) {
+				//member != null or memberLevel == 1이면 (관리자 권한 (마이페이지 or 관리자 페이지))
+				session.setAttribute("member", member);
 			}
 		}
-		return null; 
+		return "redirect:/"; 
+	}
+	@GetMapping(value = "/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/"; 
 	}
 }
