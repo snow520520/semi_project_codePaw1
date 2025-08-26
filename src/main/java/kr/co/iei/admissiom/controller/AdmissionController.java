b1cd236c6957163fe8fdcb7b6bb5ce3f9a9522b9
snow520520 <leeseolhang0624@gmail.com>
2025-08-26 10:14:38 +0900

    admission

---
+++ src/main/java/kr/co/iei/admissiom/controller/AdmissionController.java
@@ -0,0 +1,10 @@
+package kr.co.iei.admissiom.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/admission")
+public class AdmissionController {
+
+}
