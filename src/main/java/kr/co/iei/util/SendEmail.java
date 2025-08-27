69c57e048400ad29572694e1667cf807f6c5d8ac
yeah <skjb6996@naver.com>
2025-08-27 21:59:21 +0900

    commit

---
+++ src/main/java/kr/co/iei/util/SendEmail.java
@@ -0,0 +1,41 @@
+package kr.co.iei.util;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+
+@Component
+public class SendEmail {
+	
+	@Autowired
+	private JavaMailSender sender;
+	
+	public void sendMail(String emailTitle, String receiver, String emailContent) {
+		MimeMessage message = sender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+		
+		try {
+			helper.setSentDate(new Date());
+			helper.setFrom(new InternetAddress("skjb6990@gmail.com", "얍"));
+			helper.setTo(receiver);
+			helper.setSubject(emailTitle);
+			helper.setText(emailContent, true);
+			sender.send(message);
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
