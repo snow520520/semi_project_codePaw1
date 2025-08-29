8167e2dcd0d77b37f61f5088aa2262e5a24c07b8
snow520520 <leeseolhang0624@gmail.com>
2025-08-29 16:05:46 +0900

    공지사항 목록

---
+++ src/main/java/kr/co/iei/notice/controller/NoticeController.java
@@ -0,0 +1,27 @@
+package kr.co.iei.notice.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.iei.notice.model.service.NoticeService;
+import kr.co.iei.notice.model.vo.NoticeListData;
+
+@Controller
+@RequestMapping(value="/notice")
+public class NoticeController {
+	@Autowired
+	private NoticeService noticeService;
+	
+	@GetMapping(value="/list")
+	public String list(Model model, int reqPage) {
+		NoticeListData nld = noticeService.selectNoticeList(reqPage);
+		model.addAttribute("list", nld.getList());
+		model.addAttribute("pageNavi", nld.getPageNavi());
+		return "notice/list";
+	}
+}
