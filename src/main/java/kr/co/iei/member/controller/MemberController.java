b2add0a9698bc3e767224ae74c210ea217a07b0f
Ginam01 <dbrlska0105@gmail.com>
2025-09-01 14:21:39 +0900

    login cookie

--- src/main/java/kr/co/iei/member/controller/MemberController.java
+++ src/main/java/kr/co/iei/member/controller/MemberController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import kr.co.iei.member.model.service.MemberService;
 import kr.co.iei.member.model.vo.Member;
@@ -19,39 +21,55 @@ public class MemberController {
 	private MemberService memberService;
 
 	@GetMapping(value = "/loginFrm")
-	public String loginFrm() {
-		return "member/login";
+	public String loginFrm(HttpServletRequest request, Model model) {
+	    Cookie[] cookies = request.getCookies();
+	    String savedId = "";
+	    if(cookies != null) {
+	        for(Cookie c : cookies) {
+	            if(c.getName().equals("memberId")) {
+	                savedId = c.getValue();
+	            }
+	        }
+	    }
+	    model.addAttribute("savedId", savedId);
+	    return "member/login";
 	}
 
 	@PostMapping(value = "/login")
-	public String login(Member m, Model model, HttpSession session) {
-		// select * from member_tbl where member_id = memberId and member_pw = memberPw
+	public String login(Member m, boolean saveId ,Model model, HttpSession session, HttpServletResponse response) {
 		Member member = memberService.login(m);
 		
 		if (member == null) {
-			// member == null 이면 로그인이 안된경우 (id or pw가 틀렸다는 문구 다시 확인하세요)
 			 model.addAttribute("title", "로그인 실패"); 
 			 model.addAttribute("text", "아이디와 비밀번호를 확인해주세요.");
 			 model.addAttribute("icon", "error");
 			 model.addAttribute("loc", "/member/loginFrm");
 			return "common/msg";
-			// alert css/js 넣고 처리해야함
 		} else {
-
-			// member != null 로그인 된 경우
-			if (member.getMemberLevel() == 3) {
-				// member != null or memberLevel == 3이면 (정회원(입양하지 않은 회원) 마이페이지에 회원정보 수정할수 있게끔만)
-				session.setAttribute("member", member);
-
-			} else if (member.getMemberLevel() == 2) {
-				// member != null or memberLevel == 2이면 (입양한 회원 (마이페이지에 입양 후기쓴 글 or 입양신청서 작성 시
-				// 수락 / 회원정보 수정할 수 있게))
-				session.setAttribute("member", member);
-
-			} else if (member.getMemberLevel() == 1) {
-				// member != null or memberLevel == 1이면 (관리자 권한 (마이페이지 or 관리자 페이지))
-				session.setAttribute("member", member);
-			}
+			
+			// 레벨별 세션 처리
+            if (member.getMemberLevel() == 3) {
+                // 정회원 (입양하지 않은 회원)
+                session.setAttribute("member", member);
+            } else if (member.getMemberLevel() == 2) {
+                // 입양한 회원
+                session.setAttribute("member", member);
+            } else if (member.getMemberLevel() == 1) {
+                // 관리자
+                session.setAttribute("member", member);
+            }
+            
+			session.setAttribute("member", member);
+			if (saveId) {
+	            Cookie cookie = new Cookie("memberId", member.getMemberId());
+	            cookie.setPath("/");
+	            response.addCookie(cookie);
+	        } else {
+	            Cookie cookie = new Cookie("memberId", null);
+	            cookie.setMaxAge(0);
+	            cookie.setPath("/");
+	            response.addCookie(cookie);
+	        }
 		}
 		return "redirect:/";
 	}
@@ -77,10 +95,10 @@ public class MemberController {
 			return 1;
 		}
 	}
+
 	@PostMapping(value ="/join")
 	public String join(Member m, Model model) {
 		int result = memberService.join(m);
-		
 		if(result == 0) {
 			 model.addAttribute("title", "회원가입 실패"); 
 			 model.addAttribute("text", "입력한 정보를 확인해주세요.");
@@ -94,9 +112,9 @@ public class MemberController {
 		}
 		return "common/msg"; 
 	}
+
 	@GetMapping(value = "/findIdFrm")
 	public String findIdFrm() {
 		return "member/findId";
 	}
-
 }
