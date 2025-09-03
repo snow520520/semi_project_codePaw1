e8516fc6fae69d3decd0f3e8ae106a0d11bd860b
snow520520 <leeseolhang0624@gmail.com>
2025-09-03 16:01:58 +0900

    메인페이지 사진추가

--- src/main/java/kr/co/iei/member/model/service/MemberService.java
+++ src/main/java/kr/co/iei/member/model/service/MemberService.java
@@ -192,6 +192,7 @@ public class MemberService {
 	public int deleteInfo(int memberNo) {
 		int result = memberDao.deleteInfo(memberNo);
 		return result;
+	}
 	public Member selectMemberNo(int memberNo) {
 		return memberDao.selectMemberNo(memberNo);
 	}
